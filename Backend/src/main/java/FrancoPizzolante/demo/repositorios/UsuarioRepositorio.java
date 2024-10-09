c5a389779f8c43da6e273d2576b035b0ff858858
Francopizzolante <Francogpizzolantesuarez@gmail.com>
2024-10-09 09:16:08 -0300

    borar back

--- Backend/src/main/java/FrancoPizzolante/demo/repositorios/UsuarioRepositorio.java
+++
@@ -1,7 +0,0 @@
-package FrancoPizzolante.demo.repositorios;
-
-import FrancoPizzolante.demo.entidades.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-}
